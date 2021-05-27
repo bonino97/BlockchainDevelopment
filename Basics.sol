1d3198dbeb5f0531e5f08f046ab173e8116a7dff
bonino97 <juanbonino97@gmail.com>
2021-05-27 09:09:08 -0300

    Commit events

--- Basics.sol
+++ Basics.sol
@@ -1,170 +1,233 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
 
 contract Basics {
     /* integer, strings, boolean, bytes */
     /* array, enum, structs, mappings (dictionary in python) */
-    
+
     /* Integers */
-    int public normalInteger = 1; // int = int256
+    int256 public normalInteger = 1; // int = int256
     int8 public numberTwo = 2; // 2^8 = 256
-    int16 public numberThree = 3; // 2^16 = 65000 
+    int16 public numberThree = 3; // 2^16 = 65000
     uint8 public numberFour = 10; // Only positives numbers.
-    uint public msgValue;
-    
+    uint256 public msgValue;
+
     /* Boolean */
     bool public booleanVar = true;
-    
+
     /* String */
     string public stringVar = "Testing String Var";
     string public name;
-    
+
     /* Bytes */
-    bytes public byteVar = 'abc'; // 0x616263
-    
+    bytes public byteVar = "abc"; // 0x616263
+
     /* Address */
     address payable public investor;
     address public owner;
-    
+
     /* Arrays */
     string[] public dynamicArray; //Dynamic Array. Without max.
     string[3] public fixedOrStaticArray; //Fixed length array. With max length.
     //Push, pop and Length are possible in solidity.
-    
+
     /* Mappings */
-    
-    mapping(string => bool) public stringMapsToBoolean ;
-    
+
+    mapping(string => bool) public stringMapsToBoolean;
+
     /* Enum */
-    enum Countries { Argentina, EstadosUnidos, Italia }
+    enum Countries {Argentina, EstadosUnidos, Italia}
     Countries public country = Countries.Argentina;
-    
+
     /* Structs */
     struct Student {
         string name;
-        int yearsOld;
+        int256 yearsOld;
         string city;
         string street;
         string university;
-        int average;
+        int256 average;
     }
-    
+
     Student public studentOne;
     Student public studentTwo;
-    
-    int public sum = 0;
-    
+    Student public newStudent;
+
+    int256 public sum = 0;
+
     /* Constructor */
- 
-    constructor() public{ // Used to initiate values on variables.
+
+    constructor() public {
+        // Used to initiate values on variables.
         owner = msg.sender; // Deployer of the contract.
-    }    
-    
+    }
+
     /* Modifiers */
 
     modifier onlyOwner() {
-        require(msg.sender == owner, 'Only owner can see Balance.');
+        require(msg.sender == owner, "Only owner can see Balance.");
         _;
     }
-    
+
     /* Functions */
-    
-    function setSum(int fNumberOne, int fNumberTwo) public {
+
+    function setSum(int256 fNumberOne, int256 fNumberTwo) public {
         sum += fNumberOne + fNumberTwo;
     }
-    
-    function getSum() public view returns(int) { 
-         return sum;
+
+    function getSum() public view returns (int256) {
+        return sum;
     }
-    
-    function getMultipleValues() public view returns(int8, int16, int) { 
-         return (numberTwo, numberThree, numberFour);
+
+    function getMultipleValues()
+        public
+        view
+        returns (
+            int8,
+            int16,
+            int256
+        )
+    {
+        return (numberTwo, numberThree, numberFour);
     }
-    
-    function getMultipleValuesWithAnotherParameters() public view returns(int one, int two, int three) { 
-         return (numberTwo, numberThree, numberFour);
+
+    function getMultipleValuesWithAnotherParameters()
+        public
+        view
+        returns (
+            int256 one,
+            int256 two,
+            int256 three
+        )
+    {
+        return (numberTwo, numberThree, numberFour);
     }
-    
-    function getPureFunction() public pure returns(int) {
+
+    function getPureFunction() public pure returns (int256) {
         return 10; // Doesnt modify and returns nothing inside the Smart Contract, just return a Inside Function Value.
     }
-    
+
     function setString(string memory _name) public {
         name = _name;
     }
-    
-    function getString() public view returns(string memory studentName) {
+
+    function getString() public view returns (string memory studentName) {
         return name;
     }
-    
+
     function setVariables() public {
-        dynamicArray.push('Push Something!');
-        fixedOrStaticArray[1] = 'Peruzzi';
-        stringMapsToBoolean['Peruzzi'] = true;
-        
-        studentOne.name = 'Juan Cruz';
+        dynamicArray.push("Push Something!");
+        fixedOrStaticArray[1] = "Peruzzi";
+        stringMapsToBoolean["Peruzzi"] = true;
+
+        studentOne.name = "Juan Cruz";
         studentOne.yearsOld = 24;
-        studentOne.city = 'Corral de Bustos';
-        studentOne.street = 'Santa Fe';
-        studentOne.university = 'UAI';
+        studentOne.city = "Corral de Bustos";
+        studentOne.street = "Santa Fe";
+        studentOne.university = "UAI";
         studentOne.average = 6;
-        
-        Student memory tempStudent = Student({
-            name: 'Milagros',
-            yearsOld: 19,
-            city: 'Corral de Bustos',
-            street: 'Uruguay',
-            university: 'UNR',
-            average: 8
-        });
-        
+
+        Student memory tempStudent =
+            Student({
+                name: "Milagros",
+                yearsOld: 19,
+                city: "Corral de Bustos",
+                street: "Uruguay",
+                university: "UNR",
+                average: 8
+            });
+
         studentTwo = tempStudent;
     }
-    
+
     function swap() public {
-        int swapTemp = normalInteger; // No need to pay gas fee for local variables
+        int256 swapTemp = normalInteger; // No need to pay gas fee for local variables
         normalInteger = 10;
         sum = swapTemp;
     }
-    
+
     /* Payable Functions */
-    
-    function payMoney() payable public { // Can accept money from people.
+
+    function payMoney() public payable {
+        // Can accept money from people.
         //Do something
     }
-    
-    function getBalance() view public onlyOwner returns(uint){
+
+    function getBalance() public view onlyOwner returns (uint256) {
         // require(msg.sender == owner, 'Only owner can see Balance.');
         return address(this).balance / 1e18;
         // if(msg.sender == owner) {
-        //     return address(this).balance / 1e18;    
+        //     return address(this).balance / 1e18;
         // } else {
         //     revert();
         // }
     }
-    
+
     function sendMoney() public {
         investor = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
         investor.transfer(address(this).balance);
     }
-    
+
     /* Global Variables */
-    
-    function getValues() public view returns(uint blockNumber, uint blockTimestamp, address msgSender, bytes calldata msgData) {
+
+    function getValues()
+        public
+        view
+        returns (
+            uint256 blockNumber,
+            uint256 blockTimestamp,
+            address msgSender,
+            bytes calldata msgData
+        )
+    {
         // Block Number - uint -> Block number where Blockchain is working on.
         // Block TimeStamp - uint
         // Msg Sender - address -> Person who is calling this contract.
         // Msg Data - bytes
         return (block.number, block.timestamp, msg.sender, msg.data);
     }
-    
-    function payToContract() payable public {
+
+    function payToContract() public payable {
         msgValue = msg.value; // Value sended by user.
     }
-    
+
     /* Fallback */
-    fallback () external payable {
+    fallback() external payable {
         msg.sender.transfer(msg.value);
+    }
+
+    event studentRegistered(
+        string name,
+        int256 yearsOld,
+        string city,
+        string street,
+        string university,
+        int256 average
+    );
+
+    function registerStudent(
+        string memory _name,
+        int256 _yearsOld,
+        string memory _city,
+        string memory _street,
+        string memory _university,
+        int256 _average
+    ) public {
+        newStudent.name = _name;
+        newStudent.yearsOld = _yearsOld;
+        newStudent.city = _city;
+        newStudent.street = _street;
+        newStudent.university = _university;
+        newStudent.average = _average;
+
+        emit studentRegistered(
+            _name,
+            _yearsOld,
+            _city,
+            _street,
+            _university,
+            _average
+        );
     }
 }
