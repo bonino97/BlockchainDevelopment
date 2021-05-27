c4d2b69baf66cce945d363c4c22364ffd6e32e39
bonino97 <juanbonino97@gmail.com>
2021-05-27 14:45:24 -0300

    Adding more functions

--- Polymorphism.sol
+++ Polymorphism.sol
@@ -2,10 +2,14 @@
 
 pragma solidity 0.6.12;
 
-/* Polymorphism */
-
 contract Polymorphism {
+    
     function sum(int a, int b) public pure returns (int) {
         return a + b;
     }
+    
+    function sum(int a, int b, int c) public pure returns (int) {
+        return a + b + c;
+    }
+    
 }
